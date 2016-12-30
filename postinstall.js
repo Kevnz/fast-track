29807aca156cdd1d24f61aa08397ea0e7397ef63
Kevin Isom <kevinisom@Shenandoah.local>
2016-12-30 16:00:30 +1300

    post install

---
+++ postinstall.js
@@ -0,0 +1,9 @@
+console.log(`
+        . . . . o o o o o
+               _____      o       _______
+      ____====  ]OO|_n_n__][.     |fast-|
+     [________]_|__|________)<    |track|
+       oo    oo  'oo OOOO-| oo\\_  ~~~|~~~
++--+--+--+--+--+--+--+--+---+--+--+--+--+  
+`);
+console.log('Thanks for using fast-track');
