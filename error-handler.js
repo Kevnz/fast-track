defc18fb1fa9dacbaf2e2cc087e80400d270c003
Kevin Isom <kevinisom@Shenandoah.local>
2016-12-30 21:55:40 +1300

    handle prod errors and logging

--- error-handler.js
+++ error-handler.js
@@ -1,11 +1,14 @@
-module.exports = function errorHander(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: err,
-    username: 'Error',
-    title: 'There was a problem',
-    pageDescription: 'bad things happened',
-    url: req.originalUrl
-  });
+module.exports = function(logger) {
+  return function errorHander(err, req, res, next) {
+    logger(err);
+    const errorToShow = process.env.NODE_ENV === 'production' ? {} : err;
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: errorToShow,
+      title: 'There was a problem',
+      pageDescription: 'bad things happened',
+      url: req.originalUrl
+    });
+  }
 }
