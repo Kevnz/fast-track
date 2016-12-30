5cce810dcb30a8a58347ac42296bca8cd72c3b8c
Kevin Isom <kevinisom@Shenandoah.local>
2016-12-30 17:29:57 +1300

    small templates and correct directory

--- install.js
+++ install.js
@@ -1,8 +1,9 @@
 const fs = require('fs-extra');
 const path = require('path');
-const configDir = path.join(process.cwd(), 'config');
-const viewsDir = path.join(process.cwd(), 'views');
-const publicDir = path.join(process.cwd(), 'public')
+const configDir = path.join(process.cwd(), '../../config');
+const viewsDir = path.join(process.cwd(), '../../views');
+const publicDir = path.join(process.cwd(), '../../public')
+
 fs.ensureDirSync(configDir);
 fs.ensureDirSync(publicDir);
 fs.ensureDirSync(viewsDir);
@@ -33,6 +34,11 @@ const index = `
 <%- contentFor('pageTitle') %>
 <%= title %>
 `;
+const error = `
+<h1><%= message %></h1>
+<h2><%= error.status %></h2>
+<pre><%= error.stack %></pre>
+`;
 
 const config = {
   "session": {
@@ -46,7 +52,8 @@ const config = {
 }
 fs.writeFileSync(path.join(viewsDir, 'layout.ejs' ), layout, 'utf8');
 fs.writeFileSync(path.join(viewsDir, 'index.ejs'), index, 'utf8');
-  
-fs.writeJsonSync('./package.json', config);
+fs.writeFileSync(path.join(viewsDir, 'error.ejs'), error, 'utf8');
+
+fs.writeJsonSync(path.join(configDir, './config.json'), config);
 fs.copy(path.join(__dirname, 'favicon.ico'), path.join(publicDir, 'favicon.ico'));
 console.log('directories and files created');
